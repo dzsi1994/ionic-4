33331539d2b1959fd2bb5ff3b8dfec5b029d187e
Gergő Nagy <gnagy@integramedia.com>
2019-07-18 13:28:07 +0200

    change to new loadChildren syntax

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePageModule } from './home/home.module';
+import { OrderPageModule } from './order/order.module';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'order', loadChildren: './order/order.module#OrderPageModule' },
+  { path: 'home', loadChildren: () => HomePageModule },
+  { path: 'order', loadChildren: () => OrderPageModule },
 ];
 
 @NgModule({
